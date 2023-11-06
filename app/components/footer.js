b450b7cefae508cbe01a50aebf27ae484d0c5f0f
camcgreen <c.c.green@outlook.com>
2023-11-06 11:36:30 +0000

    Add footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -8,13 +8,16 @@ const Footer = () => {
     <footer className={styles.container}>
       <div className='wrapper'>
         <div className={styles.content}>
-          <p>Let&apos;s talk!</p>
           <a
             href='mailto:hello@camgreen.works'
             target='_blank'
             rel='noopener noreferrer'
+            style={{ color: 'white' }}
           >
-            hello@camgreen.works
+            ✉️ -{' '}
+            <span style={{ textDecoration: 'underline' }}>
+              hello@camgreen.works
+            </span>{' '}
           </a>
         </div>
       </div>
