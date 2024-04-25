c06acf872e937bb64183e96fa978489dca682b5e
camcgreen <c.c.green@outlook.com>
2024-04-25 11:35:45 +0100

    Replace references of engineer to developer

---
+++ app/(routes)/page.js
@@ -0,0 +1,13 @@
+import Header from '../components/header'
+import About from '../components/about'
+import Projects from '../components/projects'
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <About />
+      <Projects />
+    </main>
+  )
+}
